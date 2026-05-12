a36db223e7d1b974b3bd9e8215a23aa441ae318a
An1nhasz <ana.rodrigues.souza29@escola.pr.gov.br>
2026-05-12 10:43:19 -0300

    agagaga

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao")
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-        // Remove a classe "ativo" de todos os botões
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-        }
-        
-        // Adiciona a classe "ativo" apenas no botão que foi clicado
-        botoes[i].classList.add("ativo");
-    };
+for(let i =0; i < botoes.length; i++) {
+  botoes [i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++){
+      botoes[j].classList.remove("ativo")
+    }
+
+      botoes[i].classList.add("ativo");
+  };
 }
