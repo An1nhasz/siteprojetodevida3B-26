8f4b4c12bb4863ff09a13e93adfcbbbcf2a7a24b
An1nhasz <ana.rodrigues.souza29@escola.pr.gov.br>
2026-05-12 10:37:12 -0300

    gggg

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const botoes = document.querySelectorAll (".botao");
+const botoes = document.querySelectorAll(".botao");
 
-for (let i = 0; i < botoes.length; i++){
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++){
-          botoes[i].classList.remove("ativo");
-    }
-    
-    botoes[i].classList.add("ativo");
-  };
+        // Remove a classe "ativo" de todos os botões
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+        }
+        
+        // Adiciona a classe "ativo" apenas no botão que foi clicado
+        botoes[i].classList.add("ativo");
+    };
 }
